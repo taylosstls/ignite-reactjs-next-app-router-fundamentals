332ec370f1d2ddff2efead85c3b047552e5e30e2
Gustavo Teixeira <luisgustavogto@gmail.com>
2024-04-29 12:40:11 -0300

    Transform in Client Components

--- next-fundamentals/src/app/catalog/product/[...data]/page.tsx
+++ next-fundamentals/src/app/catalog/product/[...data]/page.tsx
@@ -1,21 +1,41 @@
+// Client Components => O JS é enviado ao navegador e passa a ter interatividade na tela
+'use client'
 interface ProductsProps {
   params: {
     data: [productId: string, size: string, color: string]
   }
 }
 
-// URL ficaria no formato:
-// site.com.br/catalog/products/"productId/size/color"
+/*
+ Server Components => A gente não usa o JavaScript no lado do cliente
+ o debug ocorre dentro do próprio NEXT server e não tem interatividade do usuário
+*/
+
 export default function Product({ params }: ProductsProps) {
+  /*
+    URL ficaria no formato:
+    site.com.br/catalog/product/"productId/size/color"
+  */
   const [productId, size, color] = params.data
 
   console.log(params)
+
+  function addToCart() {
+    console.log('Adicionou ao carrinho')
+  }
 
   return (
     <div>
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
+
+      <button
+        className="mt-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        onClick={addToCart}
+      >
+        Adicionar ao carrinho
+      </button>
     </div>
   )
 }
