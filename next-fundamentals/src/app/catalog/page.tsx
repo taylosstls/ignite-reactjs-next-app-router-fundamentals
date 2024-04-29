4d37e009745e571c270d3db2337b1e4a6f2194b9
Gustavo Teixeira <luisgustavogto@gmail.com>
2024-04-29 12:47:37 -0300

    Trazendo retorno de dados de uma API com uso de Fetch com async await

--- next-fundamentals/src/app/catalog/page.tsx
+++ next-fundamentals/src/app/catalog/page.tsx
@@ -1,3 +1,6 @@
-export default function Catalog() {
-  return <h1>Product</h1>
+export default async function Catalog() {
+  const response = await fetch('https://api.github.com/users/taylosstls')
+  const user = await response.json()
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
