975fa2a0120a11ac59bdea014bbc07fbc124c64b
Gustavo Teixeira <luisgustavogto@gmail.com>
2024-04-30 17:06:14 -0300

    Create Project Devstore

---
+++ devstore/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+export const metadata: Metadata = {
+  title: 'Devstore',
+  description: 'Loja e-commerce feita em NextJS',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.variable}>{children}</body>
+    </html>
+  )
+}
