84a738ec066887380d2e79c5f4a056484f1be7d1
Gustavo Teixeira <luisgustavogto@gmail.com>
2024-04-30 19:17:57 -0300

    Finishing header application

--- devstore/src/app/layout.tsx
+++ devstore/src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={inter.variable}>{children}</body>
+    <html className={inter.variable} lang="pt-BR">
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
