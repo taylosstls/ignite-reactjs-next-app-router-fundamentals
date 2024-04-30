8ddd029e6dc50dca4baf5d73c0e4d62b4cff48fb
Gustavo Teixeira <luisgustavogto@gmail.com>
2024-04-30 19:51:41 -0300

    Adjust bottom side from product

--- devstore/src/app/(store)/(home)/page.tsx
+++ devstore/src/app/(store)/(home)/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           quality={100}
         />
 
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 group-hover:bg-black transition-all duration-500 p-1 pl-5">
+        <div className="absolute bottom-20 right-20 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 group-hover:bg-black transition-all duration-500 p-1 pl-5">
           <span className="text-sm truncate">Moletom Never Stop Learning</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
             R$129
